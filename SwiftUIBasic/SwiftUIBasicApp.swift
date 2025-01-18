50c1cc1ab5862c74d600ab8b78e13a517f02348e
YOO TAEYANG <yty0706@gmail.com>
2025-01-19 01:14:22 +0900

    Initial Commit

---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by taeyang on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
